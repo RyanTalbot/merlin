d1aa9b23fe3bf7463b5fd961e1d3f8f7e992bc98
Ryan Talbot <ryan-talbot@outlook.com>
2022-04-12 23:03:31 +0100

    Added cleanCSS plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const cleanCSS = require('gulp-clean-css');
 
 function build() {
     return src('src/**/*.scss')
         .pipe(sass())
+        .pipe(cleanCSS())
         .pipe(dest('styles'))
 }
 
